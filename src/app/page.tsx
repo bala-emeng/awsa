c6b62d62972fb752a64b0833264dbed3793867a4
bala-emeng <47141828+bala-emeng@users.noreply.github.com>
2024-11-05 17:11:02 +0530

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className=" bg-slate-400 font-mono text-3xl h-screen w-full ">
+      
+      <button className="pl-7 pt-9">Wel come to AWS website</button>
+      <p className="pl-7 pt-9">Learn more ......</p>
+    </div>
+  );
+}
