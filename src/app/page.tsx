e295d60ebbc6c212604d8fe81cc1ee026fbd2564
bala-emeng <47141828+bala-emeng@users.noreply.github.com>
2024-11-05 17:18:23 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <div className=" bg-slate-400 font-mono text-3xl h-screen w-full ">
       
       <button className="pl-7 pt-9">Wel come to AWS website</button>
+      <p className="pl-7 pt-9">Creater</p>
       <p className="pl-7 pt-9">Learn more ......</p>
     </div>
   );
